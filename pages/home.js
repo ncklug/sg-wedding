1183f35571266f79b4983cfc28616d9ba0c227a2
ncklug <ncklug@gmail.com>
2023-05-06 00:48:41 +0800

    Door on left + cleanups

--- pages/home.js
+++ pages/home.js
@@ -2,12 +2,9 @@ import { useEffect } from 'react';
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-    useEffect(() => {}, [])
-
-
   return (
     <div className={styles.container}>
-        And that's how we met.
+    [Our story will go here]
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,66 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
-import Image from 'next/image'
-import Link from 'next/link'
-// import retroPlant from '../assets/retroImage.jpeg'
-import door from '../assets/door_6.png'
-import { useEffect, useState } from 'react';
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import door from "../assets/door_1.png";
 
 export default function Index() {
-
   return (
-    <div className={styles.container}>
+    <div
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
       <Head>
         <title>SG Wedding!</title>
       </Head>
 
-      {/* <Image src={retroPlant} className={styles.retro} /> */}
-      <div className={styles.topprogress}><Link className={styles.link} href='/home'><Image src={door} className={styles.door} /></Link><div className={styles.filler}></div></div>
-      <div>
-        <svg width='80vw' height='10vh'>
-          {/* TODO: Add dots */}
-          <svg height='50%'>
-              <rect width='100%' height='100%' fill='black'></rect>
-              <rect y='10%' x='0.5%' width='99%' height='80%' fill='white'></rect>
-              <rect y='20%' x='1%' width='95%' height='60%' fill='black'></rect>
+      <div style={{ flex: "2", display: "flex" }}>
+        <div
+          style={{ alignSelf: "center", display: "flex", alignItems: "center" }}
+        >
+          <Link
+            style={{ marginBottom: "5%", marginRight: "0.5rem" }}
+            onClick={(e) => {
+              alert("Please try again later :)");
+              e.preventDefault();
+            }}
+            href="/home"
+          >
+            <Image
+              src={door}
+              style={{
+                width: 37,
+                height: 62,
+                /* transform: perspective(300px) rotateY(-28deg) skew(0deg, 11deg) */
+              }}
+            />
+          </Link>
+          <svg width="50vw" height="10vh">
+            {/* TODO: Add dots */}
+            <svg height="50%">
+              <rect width="100%" height="100%" fill="black"></rect>
+              <rect
+                y="10%"
+                x="0.5%"
+                width="99%"
+                height="80%"
+                fill="white"
+              ></rect>
+              <rect y="20%" x="1%" width="95%" height="60%" fill="black"></rect>
+            </svg>
+            <svg y="50%" height="50%">
+              <text y="97.5%" x="89%">
+                99%
+              </text>
+            </svg>
           </svg>
-          <svg y='50%' height='50%'>
-            <text y='97.5%' x='91%'>99%</text>
-          </svg>
-          </svg>
+        </div>
       </div>
-
+      <div style={{ flex: "1" }}></div>
     </div>
   );
 }

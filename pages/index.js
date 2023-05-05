8bb50e4c37716cd6615f2fdeafe6f05c046c869e
ncklug <ncklug@gmail.com>
2023-05-06 01:36:47 +0800

    Added Chakra

--- pages/index.js
+++ pages/index.js
@@ -2,66 +2,104 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import door from "../assets/door_1.png";
+import {
+  Tooltip,
+  extendTheme,
+  ChakraProvider,
+  defineStyleConfig,
+} from "@chakra-ui/react";
+
+// define the base component styles
+const baseTooltipStyle = {};
+
+// export the component theme
+export const tooltipTheme = defineStyleConfig({ baseStyle: baseTooltipStyle });
+
+const theme = extendTheme({
+  components: {
+    Tooltip: tooltipTheme,
+  },
+});
 
 export default function Index() {
   return (
-    <div
-      style={{
-        minHeight: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        fontSize: "min(3vh, 2vw)",
-      }}
-    >
-      <Head>
-        <title>SG Wedding!</title>
-      </Head>
+    <ChakraProvider theme={theme}>
+      <div
+        style={{
+          minHeight: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          fontSize: "min(3vh, 2vw)",
+        }}
+      >
+        <Head>
+          <title>SG Wedding!</title>
+        </Head>
 
-      <div style={{ flex: "2", display: "flex" }}>
-        <div
-          style={{ alignSelf: "center", display: "flex", alignItems: "center" }}
-        >
-          <Link
-            style={{ marginBottom: "3.5em", marginRight: "1em" }}
-            onClick={(e) => {
-              alert("Please try again later :)");
-              e.preventDefault();
+        <div style={{ flex: "2", display: "flex" }}>
+          <div
+            style={{
+              alignSelf: "center",
+              display: "flex",
+              alignItems: "center",
             }}
-            href="/home"
           >
-            <Image
-              src={door}
-              style={{
-                width: "3em",
-                height: "5em",
-                /* transform: perspective(300px) rotateY(-28deg) skew(0deg, 11deg) */
-              }}
-            />
-          </Link>
-          <svg width="25em" height="5em" overflow="visible">
-            {/* TODO: Add dots */}
-            <svg height="50%">
-              <rect width="100%" height="100%" fill="black"></rect>
-              <rect
-                y="10%"
-                x="0.5%"
-                width="99%"
-                height="80%"
-                fill="white"
-              ></rect>
-              <rect y="20%" x="1%" width="95%" height="60%" fill="black"></rect>
+            <Tooltip
+              label={
+                <span style={{ fontSize: "1em" }}>Come back later :&#41;</span>
+              }
+              // placement="top"
+              bg="black"
+              color="white"
+            >
+              <Link
+                style={{ marginBottom: "3.5em", marginRight: "1em" }}
+                onClick={(e) => {
+                  e.preventDefault();
+                }}
+                href="/home"
+              >
+                <Image
+                  src={door}
+                  style={{
+                    width: "3em",
+                    height: "5em",
+                    /* transform: perspective(300px) rotateY(-28deg) skew(0deg, 11deg) */
+                  }}
+                />
+              </Link>
+            </Tooltip>
+            <svg width="25em" height="5em" overflow="visible">
+              {/* TODO: Add dots */}
+              <svg height="50%">
+                <rect width="100%" height="100%" fill="black"></rect>
+                <rect
+                  y="10%"
+                  x="0.5%"
+                  width="99%"
+                  height="80%"
+                  fill="white"
+                ></rect>
+                <rect
+                  y="20%"
+                  x="1%"
+                  width="95%"
+                  height="60%"
+                  fill="black"
+                ></rect>
+              </svg>
+              <svg y="50%" height="1.5em" overflow="visible">
+                <text y="97.5%" x="89%" dominantBaseline="ideographic">
+                  99%
+                </text>
+              </svg>
             </svg>
-            <svg y="50%" height="1.5em" overflow="visible">
-              <text y="97.5%" x="89%" dominant-baseline="ideographic">
-                99%
-              </text>
-            </svg>
-          </svg>
+          </div>
         </div>
+        <div style={{ flex: "1" }}></div>
       </div>
-      <div style={{ flex: "1" }}></div>
-    </div>
+    </ChakraProvider>
   );
 }

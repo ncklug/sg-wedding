9b89ef555a51e8c70f6010a3b1219e69829997c1
ncklug <ncklug@gmail.com>
2023-05-07 07:52:44 +0800

    v0

--- pages/index.js
+++ pages/index.js
@@ -23,9 +23,12 @@ const theme = extendTheme({
   },
 });
 
+const LOADING_DOTS = ["   ", ".  ", ".. ", "..."];
+
 export default function Index() {
   const [doorTooltipOpen, setDoorTooltipOpen] = useState(false);
   const timeoutId = useRef(undefined);
+
   useEffect(() => {
     if (doorTooltipOpen) {
       timeoutId.current = setTimeout(() => {
@@ -38,6 +41,15 @@ export default function Index() {
       }
     }
   }, [doorTooltipOpen]);
+
+  const [loadingDotsIdx, setLoadingDotsIdx] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoadingDotsIdx((prev) => prev + 1);
+    }, 800);
+  }, [loadingDotsIdx]);
+
   return (
     <ChakraProvider theme={theme}>
       <div
@@ -54,81 +66,95 @@ export default function Index() {
           <title>SG Wedding!</title>
         </Head>
 
-        <div style={{ flex: "2", display: "flex" }}>
+        <div style={{ display: "flex" }}>
           <div
             style={{
-              alignSelf: "center",
               display: "flex",
+              flexDirection: "column",
               alignItems: "center",
+              alignSelf: "center",
             }}
           >
-            <Tooltip
-              label={
-                <span style={{ fontSize: "1em" }}>Come back later :&#41;</span>
-              }
-              // placement="top"
-              bg="black"
-              color="white"
-              isOpen={doorTooltipOpen}
+            <div style={{ fontSize: "1em", marginBottom: "0.3em" }}>
+              Please come back later :&#41;
+              {/* <span style={{ display: "inline-block", width: "3em" }}>
+                {LOADING_DOTS[loadingDotsIdx % 4]}
+              </span> */}
+            </div>
+            <div
+              style={{
+                alignSelf: "center",
+                display: "flex",
+                alignItems: "center",
+              }}
             >
-              <Link
-                style={{ marginBottom: "3.5em", marginRight: "1em" }}
-                onClick={(e) => {
-                  setDoorTooltipOpen((prev) => !prev);
-                  e.preventDefault();
-                }}
-                href="/home"
+              {/* <Tooltip
+                label={
+                  <span style={{ fontSize: "1em" }}>
+                    Come back later :&#41;
+                  </span>
+                }
+                bg="black"
+                color="white"
+                isOpen={doorTooltipOpen}
+              >
+                <Link
+                  style={{ marginBottom: "3.5em", marginRight: "1em" }}
+                  onClick={(e) => {
+                    setDoorTooltipOpen((prev) => !prev);
+                    e.preventDefault();
+                  }}
+                  href="/home"
+                >
+                  <Image
+                    src={door}
+                    style={{
+                      width: "3em",
+                      height: "5em",
+                    }}
+                  />
+                </Link>
+              </Tooltip> */}
+              <svg width="25em" height="5em" overflow="visible">
+                {/* TODO: Add dots */}
+                <svg height="50%">
+                  <rect width="100%" height="100%" fill="black"></rect>
+                  <rect
+                    y="10%"
+                    x="1%"
+                    width="98%"
+                    height="80%"
+                    fill="white"
+                  ></rect>
+                  <rect
+                    y="20%"
+                    x="2%"
+                    width="74%"
+                    height="60%"
+                    fill="black"
+                  ></rect>
+                </svg>
+                <svg y="50%" height="1.5em" overflow="visible">
+                  <text y="97.5%" x="64%" dominantBaseline="ideographic">
+                    74%
+                  </text>
+                </svg>
+              </svg>
+              {/* <Link
+                style={{ marginBottom: "3em", marginLeft: "1em" }}
+                href="/wedding"
               >
                 <Image
-                  src={door}
+                  src={bablogo}
                   style={{
-                    width: "3em",
-                    height: "5em",
-                    /* transform: perspective(300px) rotateY(-28deg) skew(0deg, 11deg) */
+                    width: "3.9em",
+                    height: "3.9em",
                   }}
                 />
-              </Link>
-            </Tooltip>
-            <svg width="25em" height="5em" overflow="visible">
-              {/* TODO: Add dots */}
-              <svg height="50%">
-                <rect width="100%" height="100%" fill="black"></rect>
-                <rect
-                  y="10%"
-                  x="1%"
-                  width="98%"
-                  height="80%"
-                  fill="white"
-                ></rect>
-                <rect
-                  y="20%"
-                  x="2%"
-                  width="93%"
-                  height="60%"
-                  fill="black"
-                ></rect>
-              </svg>
-              <svg y="50%" height="1.5em" overflow="visible">
-                <text y="97.5%" x="84%" dominantBaseline="ideographic">
-                  99%
-                </text>
-              </svg>
-            </svg>
-            <Link
-              style={{ marginBottom: "3em", marginLeft: "1em" }}
-              href="/wedding"
-            >
-              <Image
-                src={bablogo}
-                style={{
-                  width: "5em",
-                  height: "5em",
-                }}
-              />
-            </Link>
+              </Link> */}
+            </div>
           </div>
         </div>
-        <div style={{ flex: "1" }}></div>
       </div>
     </ChakraProvider>
   );

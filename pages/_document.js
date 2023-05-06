ce4df68b50e7e965339ef9446c70620910a683cd
ncklug <ncklug@gmail.com>
2023-05-06 23:07:08 +0800

    Finally get fonts working

---
+++ pages/_document.js
@@ -0,0 +1,27 @@
+// pages/_document.js
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body
+        style={{
+          padding: 0,
+          margin: 0,
+          fontFamily: '"Press Start 2P", cursive',
+        }}
+      >
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}

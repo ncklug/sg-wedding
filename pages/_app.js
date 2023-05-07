452076d4dfa69c738a82530e4714b0696189f145
ncklug <ncklug@gmail.com>
2023-05-07 15:29:40 +0800

    Add Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import Head from "next/head";
 import "../styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default MyApp;

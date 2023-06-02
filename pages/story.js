3cea20da49815e7794860abc50f9ad1a021f5a5e
ncklug <ncklug@gmail.com>
2023-06-02 20:04:31 +0800

    Story draft 1

--- pages/story.js
+++ pages/story.js
@@ -26,7 +26,6 @@ export default function Home() {
     <div
       style={{
         margin: "5em",
-        height: "100vh",
         fontFamily: "'Inter', sans-serif",
         lineHeight: 1.5,
       }}
@@ -77,6 +76,46 @@ export default function Home() {
             determined to go deeper until they really understood the other
             person, and haven't stopped since...
           </Aside>
+        </Paragraph>
+        <Paragraph>
+          A few short weeks later, the pandemic closed the world's borders and
+          left them stranded apart. They began to supplement their regular video
+          chats with shows they would watch simultaneously ("Three, two, one,
+          go", he would say, to make sure they were watching the same thing at
+          the same time so their laughter and commentary was also in sync) as
+          well as video games that ended up being surprisingly helpful to build
+          understanding of their dynamics.
+        </Paragraph>
+        <Paragraph>
+          Then, suddenly, an window of opportunity. Singapore very briefly
+          opened its borders for visitors with close ties to Singaporeans, which
+          amazingly included long term relationships even though they weren't
+          engaged or married. On Christmas Day, with a broken toe, he hobbled
+          through airport security and into his quarantine hotel, where he found
+          bags of welcome gifts she left for him.
+          <Aside>
+            One of the welcome gifts was an adorable mini Christmas tree that,
+            in the midst of his disorientation, he didn't realize was real and
+            therefore needed to be watered...
+          </Aside>
+        </Paragraph>
+        <Paragraph>
+          After his time in Singapore, and as the world began to return to
+          normal, they found other ways and places to stick together. They
+          visited her friend and his former housemate &#40;who was the reason
+          they met in the first place&#41; in Tokyo, studied justice and
+          peacemaking together for 3 months in community in South Africa, read
+          innumerable hotel, hostel, and Airbnb reviews, and learned all the
+          sorts of complicated things that a couple can only learn about
+          themselves and each other in the midst of unstable circumstances.
+        </Paragraph>
+        <Paragraph>
+          Over time, it became clear that the differences that they (mostly he)
+          wondered might drive them apart actually catalyzed growth in each of
+          them in areas that had been stuck for decades. While they still have a
+          lot to learn to fully thrive together, it's clear to both of them at
+          this point that they couldn't have found a better lifelong
+          collaborator...
         </Paragraph>
       </div>
     </div>

b833f24b8b040009508a43587706922bd44340c0
ncklug <ncklug@gmail.com>
2023-06-24 11:02:06 +0800

    Adding qrs

--- components/ArtistStatement.js
+++ components/ArtistStatement.js
@@ -1,13 +1,7 @@
 import { Paragraph } from "../components/Paragraph"
 import Image from "next/image"
 
-export const ArtistStatement = ({
-  paragraphs,
-  title,
-  materials,
-  qr,
-  showQr,
-}) => (
+export const ArtistStatement = ({ paragraphs, title, materials, qr, mode }) => (
   <div
     style={{
       fontFamily: "'Inter', sans-serif",
@@ -17,7 +11,7 @@ export const ArtistStatement = ({
     <div
       style={{
         paddingTop: "3em",
-        backgroundColor: "black",
+        backgroundColor: mode ? "white" : "black",
         width: "100%",
         height: "100dvh",
       }}
@@ -29,13 +23,17 @@ export const ArtistStatement = ({
           backgroundColor: "white",
         }}
       >
-        <div style={{ padding: "3.5em" }}>
-          {title}
-          <div style={{ margin: "0.3em 0 0.3em", fontSize: "1.2em" }}>
-            June 2023
-          </div>
-          <div style={{ margin: "0 0 2.8em" }}>{materials}</div>
-          {showQr ? (
+        <div style={{ padding: mode ? "0.5em" : "3.5em" }}>
+          {mode === "printText" ? null : (
+            <div>
+              {title}
+              <div style={{ margin: "0.3em 0 0.3em", fontSize: "1.2em" }}>
+                June 2023
+              </div>
+              <div style={{ margin: "0 0 2.8em" }}>{materials}</div>
+            </div>
+          )}
+          {mode === "printQr" ? (
             <Image src={qr} style={{ width: "100%", height: "100%" }} />
           ) : (
             paragraphs.map((paragraph) => (
---
+++ pages/art/pt1.js
@@ -0,0 +1,26 @@
+import Image from "next/image"
+import qr from "../../assets/qr/pt1.svg"
+import { ArtistStatement } from "../../components/ArtistStatement"
+
+export default function Catworks() {
+  return (
+    <ArtistStatement
+      title={
+        <div>
+          <div style={{ fontWeight: 800, fontSize: "1.2em" }}>
+            “Welcome to uwuvu”
+          </div>
+        </div>
+      }
+      materials="Paper wristband"
+      qr={qr}
+      //   mode="printQr"
+      paragraphs={[
+        `This is the first in a three-part interactive series (followed by “Community Collage” and “Let Love Bloom How It May”). This wristband is intended to be evocative of the kind of wristband used for entry into events, and here has a double meaning.`,
+        `On the surface, it literally marks your entry into this wedding, which we intend to theme as a premier of an art exhibition representing our lives and relationship. So much of a real relationship cannot be described with words or even captured with pictures. We did our best to design a semi-immersive exhibition space where we can try to convey the essence of our relationship through a varied collection of art pieces in different mediums.`,
+        `You’re likely reading this right as you enter, so we invite you to take this time to pause and reflect on whatever experiences you may have with Faith and Nathan or their families in preparation for the ceremony to start.`,
+        `On a deeper level, as the first in a three-part interactive series on friendship, this work is designed to capture the moment when a potential friend-to-be invites you into their life and into their world. Whether that be a simple invitation to coffee, an unexpected moment of vulnerability between meetings at work, or an especially meaningful group activity, these moments are what this wristband is designed to represent: an invitation to enter another person’s inner world.`,
+      ]}
+    />
+  )
+}
